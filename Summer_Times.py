675a863e192c8d98a38c92c2b93f1febf64add2d
sanidhyasinha098-creator <sanidhyasinha098@gmail.com>
2026-06-19 22:26:14 +0530

    Wrtie a Temprature to decide Rohan to wear Jacket or not.

--- Summer_Times.py
+++ Summer_Times.py
@@ -6,7 +6,10 @@ while True:
 
     Weather_Input = int(user_input)
 
-    if Weather_Input < 29:
-        print("Wear Jacket, It's Cold Outside\n")
+    if Weather_Input < 10:
+        print("Wear Jacket, It's Cold Outside\n") 
+    elif Weather_Input >= 10 and Weather_Input < 16:
+        print("Wear a Light Jacket, It's Cool Outside\n")
     else:
         print("No Jacket is Required\n")
+
